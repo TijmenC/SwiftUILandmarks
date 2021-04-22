0d503a32a3943011e1747e6ca148d59a2fef2b5d
Tijmen <70951009+TijmenC@users.noreply.github.com>
2021-04-22 15:23:07 +0200

    Added swiftUI

---
+++ SwiftUILandmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  SwiftUILandmarks
+//
+//  Created by Tijmen on 22/04/2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 7 )
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUILandmarks/SwiftUILandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarksApp.swift
+//  SwiftUILandmarks
+//
+//  Created by Tijmen on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
