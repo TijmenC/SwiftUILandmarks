1e898ef0f60e274a7f4d1404ff47595c8858f4b8
Tijmen <70951009+TijmenC@users.noreply.github.com>
2021-04-23 10:19:41 +0200

    Added new resources + modeldata

---
+++ SwiftUILandmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  SwiftUILandmarks
+//
+//  Created by Tijmen on 23/04/2021.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
