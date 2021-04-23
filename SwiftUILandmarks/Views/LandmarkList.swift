9c26124ffb59fda302f2890a24415e9dc1a8d691
Tijmen <70951009+TijmenC@users.noreply.github.com>
2021-04-23 11:27:09 +0200

    Added Previews

--- SwiftUILandmarks/Views/LandmarkList.swift
+++ SwiftUILandmarks/Views/LandmarkList.swift
@@ -1,27 +1,24 @@
-//
-//  LandmarkList.swift
-//  SwiftUILandmarks
-//
-//  Created by Tijmen on 23/04/2021.
-//
-
 import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
         NavigationView {
-        List(landmarks) { landmark in
-            NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                LandmarkRow(landmark: landmark) }
-            
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
-        }
-        .navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
